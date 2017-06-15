a262cbe11041c830d5300ff3e3b154179e545630
joristaglio <jettori88@gmail.com>
2017-06-15 16:14:08 -0400

    Almost finished with model, figuring out how to splice last layers for downsampling

--- image_featurizer/image_featurizer.py
+++ image_featurizer/image_featurizer.py
@@ -12,40 +12,26 @@ class ImageFeaturizer:
 
     def __init__(self,
                 images_files = None,
-                model_string = "InceptionV3"
                 scaled_size = (299, 299),
                 crop_size = (299, 299),
                 number_crops = 1,
                 random_crop = False
                 isotropic_scaling = True,
                 top_layer = True,
-                pool_features = False
+                depth_of_output = 2,
+                downsample_features = False
                 num_pooled_features = 1024
                 ):
 
-        # The pretrained models that can be loaded through Keras. The InceptionV3
-        # network is loaded into this package– using any others will automatically
-        # load the weights online.
-        list_of_possible_models = ['InceptionV3', 'Xception', 'VGG16', 'VGG19',
-                                   'ResNet50']
+
+        #### TYPE CHECKING ####
 
         # A dictionary of the boolean set for error-checking
         dict_of_booleans = {'random_crop': random_crop, 'isotropic_scaling': isotropic_scaling
-                            'top_layer': top_layer, 'pool_features': pool_features}
+                            'top_layer': top_layer, 'downsample_features': downsample_features}
 
-        # Type checking
         if image_files == None:
             raise ValueError('Image files required.')
-
-        if not isinstance(model_string, str):
-            raise ValueError('model_string is not a string! Please pass a string \
-                            of one of the available models: \
-                            InceptionV3, Xception, VGG16, VGG19, or ResNet50.')
-
-        if not model_string in list_of_possible_models:
-            raise ValueError('model_string is not one of the available models: \
-                            InceptionV3, Xception, VGG16, VGG19, or ResNet50. \
-                            Please reformulate string')
 
         if not isinstance(scaled_size, tuple):
             raise ValueError('scaled_size is not a tuple! Please list dimensions as a tuple')
@@ -57,35 +43,112 @@ class ImageFeaturizer:
             raise ValueError('number_crops is not an integer! Please specify the \
                             number of random crops you would like to average')
 
+        if not isinstance(depth_of_output, int)
+            raise ValueError('depth_of_output is not an integer! Please specify the \
+                            number of layers you would like to remove from the top \
+                            of the network for featurization. Can be between 1 and 4.')
+
         for key in dict_of_booleans:
             if not isinstance(dict_of_booleans[key], bool):
                 raise ValueError(key + ' is not a boolean! Please set to True or False, \
                                  or leave blank for default configuration')
 
+        if not isinstance(num_pooled_features, int):
+                raise ValueError('num_pooled_features is not an integer! Please set \
+                                to an integer. Recommended value is 1024, but can \
+                                be set to any integer divisor of the number of \
+                                unpooled features.')
 
-        # Create the model!
-        model = InceptionV3(weights=None)
-        model.load_weights('../model/inception_v3_weights_tf_dim_ordering_tf_kernels.h5')
 
+        #### HELPER FUNCTIONS ####
 
-        # If top_layer is set to True, we just use the top layer of the network
-        if top_layer:
-            # Pop off the last layer and get rid of the links
-            model.layers.pop()
+        def decapitate_model(model, depth):
+            '''
+            This is a method that cuts off end layers of a model equal to the depth
+            of the desired outputs, and then removes the links connecting the new
+            outer layer to the old ones.
+
+            ## Parameters: ###
+                model: The model being decapitated
+
+                depth: The number of layers to pop off the top of the network
+            '''
+
+            # Pop the layers
+            for layer in range(depth):
+                model.layers.pop()
+
+            # Break the connections
             model.outputs = [model.layers[-1].output]
+
             model.layers[-1].outbound_nodes = []
 
-        # Otherwise, we use a deeper layer as the output
-        # TODO: Should we be able to decide which deep layer?
-        else:
-            x
+
+        def downsample_features(layer, size_of_downsample):
+
+
+
+        #### BUILDING THE MODEL ####
+
+        # Initialize the model
+        model = InceptionV3(weights=None)
+        model.load_weights('../model/inception_v3_weights_tf_dim_ordering_tf_kernels.h5')
+        model_input = model.input
+
+        # Choosing model depth:
+        # If top_layer is set to True or depth is 1, we just use the top layer of the network
+        if top_layer or depth_of_output==1:
+            # Output layer here should already be (None, 2048)
+            decapitate(model, 1)
+
+        # Otherwise, we decapitate the model to the appropriate layers
+        elif depth_of_output == 2:
+            decapitate_model(model, 19)
+            x = model.layers[-1].output
+
+            # Output layer after global average should be (None, 2048)
+            x = GlobalAveragePooling2D(name='avg_pool')(x)
+
+
+        elif depth_of_output == 3:
+            decapitate_model(model, 33)
+            x = model.layers[-1].output
+
+            # Output layer after global average should be (None, 2048)
+            x = GlobalAveragePooling2D(name='avg_pool')(x)
+
+        elif depth_of_output == 4:
+            decapitate_model(model, 50)
+            x = model.layers[-1].output
+
+            # The output after global average should be (None, 1280)
+            x = GlobalAveragePooling2D(name='avg_pool')(x)
+
+
+        model = Model(input=model_input, output=x)
+        # Check output length
+        if not len(model.layers[-1].output_shape) == 2:
+            raise ValueError('Something wrong with output! Should be a tuple of \
+                            length 2, with the second value being equal to the \
+                            number of features desired. It is not of length 2.')
+
 
         # If we are pooling the features, we add a pooling layer to the outputs
-        # to bring it to the correct size. TODO: Should correct size be customizable,
-        # or just left at 1024?
+        # to bring it to the correct size.
+        if downsample_features:
+            shape_out = model.layers[-1].output_shape
 
-        if pool_features:
-            x
+            # Check that the number of downsampled features is an integer divisor
+            # of the original output
+            if not isinstance(shape_out[1]/num_pooled_features, int):
+                raise ValueError('The desired number of pooled features is not an \
+                                integer divisor of the regular output! Output shape \
+                                = ' + str(shape_out[1]) + '. The desired features \
+                                = '+ str(num_pooled_features) + '.')
+
+            # Find the pooling constant: I.E. If we want half the number of features,
+            # pooling constant will be 2.
+            pooling_constant = shape_out[1]/num_pooled_features
 
 
 
