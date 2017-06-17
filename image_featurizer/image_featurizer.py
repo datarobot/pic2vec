b4f3c68feb904a3c4b7205191f3c41f6fa28b349
joristaglio <jettori88@gmail.com>
2017-06-17 13:25:25 -0400

    Cleanup of build_featurizer, and separate function to check downsampling

--- image_featurizer/image_featurizer.py
+++ image_featurizer/image_featurizer.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
-from keras.applications.inception_v3 import InceptionV3
+from build_featurizer import build_featurizer
 from keras.layers.merge import average
 
 class ImageFeaturizer:
@@ -12,51 +12,51 @@ class ImageFeaturizer:
 
 
     def __init__(self,
-                images_files = None,
+                image_files = None,
                 scaled_size = (299, 299),
                 crop_size = (299, 299),
                 number_crops = 0,
                 random_crop = False,
                 isotropic_scaling = True,
-                depth_of_featurizer = 1,
-                downsample_features = False,
-                num_pooled_features = 1024
+                depth = 1,
+                downsample = False,
+                downsample_size = None
                 ):
 
-    '''
-    Initializer:
+        '''
+        Initializer:
 
-    Loads an initial InceptionV3 pretrained network, decapitates it and downsamples
-    according to user specifications. Loads image file and csv, and stores model
-    to perform image featurization.
+        Loads an initial InceptionV3 pretrained network, decapitates it and downsamples
+        according to user specifications. Loads image file and csv, and stores model
+        to perform image featurization.
 
-    ### Parameters: ###
-        images_files: The file containing the images
+        ### Parameters: ###
+            images_files: The file containing the images
 
-        scaled_size: The size that the images get scaled to
+            scaled_size: The size that the images get scaled to
 
-        crop_size: If the image gets cropped, decides the size of the crop
+            crop_size: If the image gets cropped, decides the size of the crop
 
-        number_crops: If 0, no cropping. Otherwise, the number of crops taken of the image
+            number_crops: If 0, no cropping. Otherwise, the number of crops taken of the image
 
-        random_crop: If False, only take the center crop. If True, take random crops
+            random_crop: If False, only take the center crop. If True, take random crops
 
-        isotropic_scaling: If False, the image is scaled non-proportionally. If true,
-                       image is scaled keeping proportions, and then cropped to correct size
+            isotropic_scaling: If False, the image is scaled non-proportionally. If true,
+                           image is scaled keeping proportions, and then cropped to correct size
 
-        depth_of_featurizer: How many layers deep we're taking the model
+            depth: How many layers deep we're taking the model
 
-        downsample_features: If True, feature layer is downsampled
+            downsample: If True, feature layer is downsampled
 
-        num_pooled_features: If feature layer is downsampled, chooses number of features
-                            to downsample it to
-    '''
+            downsample_size: If feature layer is downsampled, chooses number of features
+                                to downsample it to
+        '''
 
         #------------------------------------------------#
                     ### ERROR CHECKING ###
         # A dictionary of the boolean set for error-checking
         dict_of_booleans = {'random_crop': random_crop, 'isotropic_scaling': isotropic_scaling,
-                            'downsample_features': downsample_features}
+                            'downsample': downsample}
 
         if image_files == None:
             raise ValueError('Image files required.')
@@ -71,8 +71,8 @@ class ImageFeaturizer:
             raise TypeError('number_crops is not an integer! Please specify the' +
                             ' number of random crops you would like to average')
 
-        if not isinstance(depth_of_featurizer, int)
-            raise TypeError('depth_of_featurizer is not an integer! Please' +
+        if not isinstance(depth, int):
+            raise TypeError('depth is not an integer! Please' +
                             'specify the number of layers you would like to ' +
                             'remove from the top of the network for featurization.'+
                             ' Can be between 1 and 4.')
@@ -82,22 +82,23 @@ class ImageFeaturizer:
                 raise TypeError(key + ' is not a boolean! Please set to True '+
                                 'or False, or leave blank for default configuration')
 
-        if not isinstance(num_pooled_features, int):
-                raise TypeError('num_pooled_features is not an integer!' +
-                                ' Please set to an integer. Recommended value ' +
-                                'is 1024, but can be set to any integer divisor' +
+        if not (isinstance(downsample_size, int) or isinstance(downsample_size, type(None))):
+                raise TypeError('Tried to set downsample_size to a non-integer value!' +
+                                ' Please set to an integer or leave uninitialized.'+
+                                ' Recommended value is 1024, but can be set to any integer divisor' +
                                 ' of the number of unpooled features.')
 
         #----------------------------------------------------------------------#
 
 
-        ---###### BUILDING THE MODEL ######---
-        model = build_featurizer(depth_of_featurizer, downsample_features,
-                                 num_pooled_features)
+        ###### BUILDING THE MODEL ######
+        print "\nBuilding the featurizer!"
+        model = build_featurizer(depth, downsample,
+                                 downsample_size)
 
         # Images
         self.image_files = image_files
-        self.vectorized_images = vectorized_images
+        self.vectorized_images = None
         self.processed_images=  image_files
 
         # ADD THIS IF WE MAKE CHOICE OF MODEL
@@ -113,3 +114,4 @@ class ImageFeaturizer:
 
         # Save the model
         self.model = model
+        self.visualize=model.summary
