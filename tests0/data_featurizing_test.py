d77c2e13c38a82ceacb978aa751cef9d1cbbcd07
joristaglio <jettori88@gmail.com>
2017-06-27 15:11:44 -0400

    Improved examples

---
+++ tests0/data_featurizing_test.py
@@ -0,0 +1,97 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, Dense, Activation, Flatten
+
+import pytest
+import random
+import filecmp
+import os
+
+import pandas as pd
+import numpy as np
+
+from image_featurizer.data_featurizing import featurize_data, features_to_csv
+
+
+
+def test_featurize_data():
+    '''
+    Test that the featurize_data model correctly outputs the features of a toy
+    network on a toy tensor
+    '''
+    np.random.seed(5102020)
+
+    # Create model
+    model = Sequential([
+        Conv2D(5, (3, 3), input_shape=(5,5,3), activation='relu'),
+        Flatten(),
+        Dense(5)
+        ])
+
+    # Create the checked array
+    init_array = np.ones((5,5,5,3))
+
+    for i in xrange(5):
+        init_array[i]  = init_array[i]*i
+
+    error_array = np.ones((5,5,10))
+
+    # Check that it raises errors with badly formatted models or arrays
+    with pytest.raises(TypeError):
+        featurize_data(model, 4)
+    with pytest.raises(TypeError):
+        featurize_data(4, init_array)
+    with pytest.raises(ValueError):
+        featurize_data(model, error_array)
+
+    # Check the prediction vs. the saved array
+    check_array = np.load('tests/data_featurizing_testing/array_testing/check_featurize.npy')
+    assert np.array_equal(featurize_data(model, init_array), check_array)
+
+def test_features_to_csv():
+    '''
+    Test that the model creates the correct csvs from a toy array, csv, and
+     image list
+    '''
+
+    # The paths to the toy csvs
+    check_csv_images = 'tests/data_featurizing_testing/csv_testing/featurize_data_check_csv_images'
+    check_csv_full = 'tests/data_featurizing_testing/csv_testing/featurize_data_check_csv_full'
+    check_csv_features_only = 'tests/data_featurizing_testing/csv_testing/featurize_data_check_csv_features_only'
+    # Build the array to treat as the "full featurized data"
+    check_array = np.array([[1.,2.,3.],
+                            [4.,5.,6.],
+                            [0.,0.,0.],
+                            [7.,8.,9.]
+                            ])
+
+    # An error array with the wrong size
+    error_array = np.zeros((4,3,2))
+
+    # The image list from the csv
+    check_image_list = ['borges.jpg', 'arendt.bmp', 'sappho.png']
+
+    # Check and remove the generated csvs if they already exist
+    if os.path.isfile('{}_full'.format(check_csv_images)):
+        os.remove('{}_full'.format(check_csv_images))
+    if os.path.isfile('{}_features_only'.format(check_csv_images)):
+        os.remove('{}_features_only'.format(check_csv_images))
+
+    # Raise errors with badly formatted input: a bad array, or an image column header
+    # not present in the csv
+    with pytest.raises(ValueError):
+        error_df = features_to_csv(check_array, check_csv_images, 'derp',check_image_list)
+    with pytest.raises(ValueError):
+        error_df = features_to_csv(error_array, check_csv_images, 'images',check_image_list)
+
+    # Create the test
+    full_test_dataframe = features_to_csv(check_array, check_csv_images, 'images',check_image_list)
+
+
+    # Assert that the dataframe returned is correct, and the csv was generated correclty
+    assert np.array_equal(full_test_dataframe, pd.read_csv(check_csv_full))
+    assert filecmp.cmp('{}_features_only'.format(check_csv_images), check_csv_features_only)
+    assert filecmp.cmp('{}_full'.format(check_csv_images),check_csv_full)
+
+    # Remove the generated files
+    os.remove('{}_full'.format(check_csv_images))
+    os.remove('{}_features_only'.format(check_csv_images))
---
+++ tests0/image_featurizer_test.py
@@ -0,0 +1,95 @@
+from image_featurizer.image_featurizer import ImageFeaturizer
+import numpy as np
+import shutil
+import os
+import pytest
+
+def test_ImageFeaturizer():
+    '''
+    Test the featurizer raises the necessary errors and performs its functions correctly
+    '''
+
+    def test_featurizer_class(featurizer,
+                              downsample_size,
+                              image_column_header,
+                              automatic_downsample,
+                              csv_path,
+                              image_list,
+                              scaled_size,
+                              depth,
+                              featurized_data,
+                              data):
+
+        '''
+        This internal method simple checks the necessary assertions for
+        a featurizer image
+        '''
+        assert featurizer.downsample_size == downsample_size
+        assert featurizer.image_column_header == image_column_header
+        assert featurizer.automatic_downsample == automatic_downsample
+        assert featurizer.csv_path == csv_path
+        assert featurizer.image_list == image_list
+        assert featurizer.scaled_size == scaled_size
+        assert featurizer.depth == depth
+        assert np.array_equal(featurizer.featurized_data, featurized_data)
+        assert np.array_equal(featurizer.data, data)
+
+    check_features = np.load('tests/ImageFeaturizer_testing/check_prediction_array_1_2048.npy')
+    check_data_array = np.load('tests/ImageFeaturizer_testing/check_data_array.npy')
+    test_csv_name = 'tests/ImageFeaturizer_testing/csv_tests/generated_images_csv_test'
+
+    # Remove path to the generated csv
+    if os.path.isdir('tests/ImageFeaturizer_testing/csv_tests/'):
+        shutil.rmtree('tests/ImageFeaturizer_testing/csv_tests/')
+
+    # Raise error if depth is not an integer
+    with pytest.raises(TypeError):
+        f = ImageFeaturizer(depth=1.)
+    # Raise error if depth not 1, 2, 3, or 4
+    with pytest.raises(ValueError):
+        f = ImageFeaturizer(depth=5)
+
+    # Raise error if downsample isn't a boolean
+    with pytest.raises(TypeError):
+        f = ImageFeaturizer(automatic_downsample='True')
+    with pytest.raises(TypeError):
+        f = ImageFeaturizer(automatic_downsample=4)
+    with pytest.raises(TypeError):
+        f = ImageFeaturizer(automatic_downsample=(True,True))
+
+    # Raise error if downsample_size isn't an integer
+    with pytest.raises(TypeError):
+        f = ImageFeaturizer(downsample_size = 1.)
+
+
+
+    # Check initialization
+    f = ImageFeaturizer()
+    test_featurizer_class(f, 0, '',False, '', '', (0,0),1,np.zeros((1)),np.zeros((1)))
+
+    # Raise error if attempting to featurize before loading data
+    with pytest.raises(IOError):
+        f.featurize()
+
+
+    # Check loading the data and test that the directory path works without a '/' at the end
+    f.load_data('images',image_path='tests/feature_preprocessing_testing/test_images',\
+                new_csv_name=test_csv_name)
+    test_featurizer_class(f, 0, 'images',False, test_csv_name,\
+        ['arendt.bmp','borges.jpg','sappho.png'], (299,299),1,np.zeros((1)),check_data_array)
+
+    # Check featurization
+    f.featurize()
+
+    test_featurizer_class(f, 0, 'images',False, test_csv_name,\
+        ['arendt.bmp','borges.jpg','sappho.png'], (299,299),1,check_features,check_data_array)
+
+    # Check load and featurize at once
+    f = ImageFeaturizer()
+    f.load_and_featurize_data('images',image_path='tests/feature_preprocessing_testing/test_images/',\
+                new_csv_name=test_csv_name)
+    test_featurizer_class(f, 0, 'images',False, test_csv_name,\
+        ['arendt.bmp','borges.jpg','sappho.png'], (299,299),1,check_features,check_data_array)
+
+    # Remove the created csv after test finished
+    shutil.rmtree('tests/ImageFeaturizer_testing/csv_tests/')
