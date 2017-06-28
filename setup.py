1cb55d06cd11da48a94af8250e4ce9d551ef5b37
Michael Odintsov <michael@datarobot.com>
2017-06-28 14:55:31 +0300

    no commented out code

--- setup.py
+++ setup.py
@@ -49,11 +49,6 @@ setup(
         ]
     },
     include_package_data=True,
-    # package_data = {
-    #     'model' :
-    #         ['inception_v3_weights_tf_dim_ordering_tf_kernels.h5'
-    #         ]
-    # },
     install_requires=requirements,
     license="BSD license",
     zip_safe=False,
