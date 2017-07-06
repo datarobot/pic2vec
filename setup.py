353294a0c33a4624d81517ce6d76618603cd5da3
joristaglio <jettori88@gmail.com>
2017-07-06 16:41:16 -0400

    pypi

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ test_requirements = [
 
 setup(
     name='pic2vec',
-    version='0.4.3',
+    version='0.5.0',
     description='Featurize images using a decapitated, pre-trained deep learning network',
     long_description=readme + '\n\n' + history,
     author='Jett Oristaglio',
